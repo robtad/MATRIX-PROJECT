004ddbc75d8092e28c91ebc6019fc76977efe817
massyakur <73303954+massyakur@users.noreply.github.com>
2022-01-24 13:06:09 +0300

    for more than 1 matrix

---
+++ main_all.py
@@ -0,0 +1,29 @@
+from matrix_analyzer import *
+
+# change these
+general_file_name = 'GF3_0xb_3x3_matris' # without the extension (.txt) and the number at the beginning 
+i = 31 # the list start from 
+no_in_list = 47 # number doesn't exist in list
+
+file_list = []
+for x in range(18):
+    if i != no_in_list:
+        file_name = str(i) + general_file_name
+        file_list.append(file_name)
+    i += 1
+
+for item in file_list:
+    matrix(item)
+    write_list(item)
+    write_ordered_list(item)
+    write_rep_matrix(item)
+    
+    # reset lists
+    list.clear()
+    new_list.clear()
+    newer_list.clear()
+    sorted_list.clear()
+    new_sorted_list.clear()
+    rep_matrix.clear()
+
+    #print(list);print(new_list);print(newer_list);print(sorted_list);print(new_sorted_list);print(rep_matrix)
